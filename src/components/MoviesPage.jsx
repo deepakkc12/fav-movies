5012c5f4eb5edfe75428491541f0d9c6b30ccd3e
deepakkc12 <deepakkc8086@gmail.com>
2024-08-11 22:43:34 +0530

    env variable added

--- src/components/MoviesPage.jsx
+++ src/components/MoviesPage.jsx
@@ -11,8 +11,7 @@ const MoviesPage = () => {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNTYyOWQyOGMwMzA1ZGFhM2VmNmQyODg4MTY4OTFlMSIsIm5iZiI6MTcyMzM4OTcyNi4zOTU5MjYsInN1YiI6IjY1ZDIyZjNmZGI3MmMwMDE2MzM5NTU2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lYZw31s5Q9CPb4YDnNmXGMV3CuSumcQ8dmnr2DA0Nc4",
+      Authorization: `Bearer ${process.env.API_KEY}`,
     },
   };
 
