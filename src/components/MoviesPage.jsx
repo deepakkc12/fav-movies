109677844b1588864f2a3b2ff11b6217a3665299
deepakkc12 <deepakkc8086@gmail.com>
2024-08-11 22:51:05 +0530

    mf

--- src/components/MoviesPage.jsx
+++ src/components/MoviesPage.jsx
@@ -17,6 +17,7 @@ const MoviesPage = () => {
   };
 
   useEffect(() => {
+    console.log("hellooo");
     fetch(
       "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
       options
